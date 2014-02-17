b7553befa3838f3ad0d709e343f021b8c269b85f
Ben Lewis <bennlewis@gmail.com>
2014-02-17 10:29:15 -0700

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwelveStepper::Application.config.secret_key_base = 'c734749c3b22e311a5a190124097cd22dbfd6596e07764108bdd4f793ba6b737df82b6540efe675e1ad856d6bcfe4152673998eefce4653632300c3e31317251'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwelveStepper::Application.config.session_store :cookie_store, key: '_twelve_stepper_session'
