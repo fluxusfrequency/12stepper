b73315b1d67a90f465655058a870151a551af463
Ben Lewis <bennlewis@gmail.com>
2014-02-18 09:51:42 -0700

    built http_cache and flash responders

---
+++ responders/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'c048c43b2e540bf79d41752368304ef46f48ff352ce33902c87d853ed070777fad3ed4e8532cdaab171ce51be072807f9ba51fa5482a8d453ca48394c773376f'
