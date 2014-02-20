01fdcf91a8c947a0fe4cdd9a16b7ddf0285be16a
Ben Lewis <bennlewis@gmail.com>
2014-02-19 17:08:37 -0700

    french chapters 1-4

--- app/controllers/big_book/chapters_controller.rb
+++ app/controllers/big_book/chapters_controller.rb
@@ -3,4 +3,21 @@ class BigBook::ChaptersController < ApplicationController
     @chapters = BigBook::Chapter.all
   end
 
+  # def edit
+  #   @chapter = BigBook::Chapter.find(params[:id])
+  # end
+
+  # def update
+  #   @chapter = BigBook::Chapter.find(params[:id])
+  #   I18n.locale = :fr
+  #   @chapter.update_attributes(title: params[:big_book_chapter][:title])
+  #   @chapter.update_attributes(body: params[:big_book_chapter][:body])
+  #   @chapter.save
+  #   redirect_to root_path, :notice => "success!"
+  # end
+
+  # def chapter_params
+  #   params.require(:big_book_chapter).permit(:title, :body)
+  # end
+
 end
