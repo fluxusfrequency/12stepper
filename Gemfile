b7cafe1a48bca15d39d55e8d4b8d79461aee41f3
Ben Horne <benhorne44@gmail.com>
2014-02-20 20:39:49 -0700

    paginates worksheet pages for a step

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'bcrypt-ruby'
 gem 'yaml_db', github: 'jetthoughts/yaml_db', ref: 'fb4b6bd7e12de3cffa93e0a298a1e5253d7e92ba'
 gem 'globalize'
 gem 'meeting_finder', '0.1.1'
+gem 'kaminari'
 
 group :development, :test do
   gem 'quiet_assets'
--- app/controllers/worksheet_entries_controller.rb
+++ app/controllers/worksheet_entries_controller.rb
@@ -1,6 +1,9 @@
 class WorksheetEntriesController < ApplicationController
 
   def new
-    @entry = Entry.new
+    @entry = Entry.new(step: params[:step].to_i)
+    @worksheets = Kaminari.paginate_array(Worksheet.where(step: params[:step].to_i)).page(params[:page]).per(1)
+
+
   end
 end
