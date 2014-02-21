865d76b7b852ccfa2d5ec3ee6ea11cdb4a43bf7e
Billy <navyosu@gmail.com>
2014-02-20 22:24:25 -0700

    Changed users#create to redirect to root_path, not user_path(@user) which doesn't exist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    # redirect_to user_path(user)
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "Your account has been created"
-      redirect_to user_path(@user)
+      redirect_to root_path
     else
       render "new"
     end
