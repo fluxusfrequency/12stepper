98c7ad1366deae27dbb0b2e79cc6b118c8f6056c
Ben Lewis <bennlewis@gmail.com>
2014-02-21 16:33:53 -0700

    styling flash and working on ajax call in entry search

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -26,18 +26,20 @@
 
     function buildResultView(result, locale) {
       var view = '<div class="row" id="entry-index-item-row">' + 
-        '<div class="col-lg-2 col-md-2"><h3 class="entry-item-title">' + 
-        '<a href="/en/entries">' + result['title'] + '</a></h3>' + 
-        '<img class="img-responsive" src="http://placehold.it/165x70" alt="">' +
-        '</div><div class="col-lg-8 col-md-8" id="entry-index-item-body"><p>' + 
-        result['body'] + '</p></div><div class="col-lg-2 col-md-2">' + 
-        '<ul id="entry-item-actions"><li><a href="/' + locale + '/entries/' + 
-        result['id'] + '/edit">' + 'Edit</a></li><li><a data-method="delete"' +
-        'href="/' + locale + '/entries/' + 
-        result['id'] + '" rel="nofollow">Delete</a></li><li><a href="#">' + 
-        'Share</a></li></ul>' + '</div></div><hr />';
+      '<div class="col-lg-3 col-md-3">' +
+      '<img class="entry-token" src="" />' +
+      '<h4 class="entry-item-title"><a href="/' + locale + '/entries/' + 
+      result['id'] + '/edit">' + result['title'] + '</a></h4>' +
+      '<p class="entry-date">' + result['created_at'].slice(0, 10) + '</p></div>' + 
+      '<div class="col-lg-7 col-md-7" id="entry-index-item-body"><p>' + 
+      result['body'] + '</p></div><div class="col-lg-2 col-md-2">' + 
+      '<ul id="entry-item-actions"><li><a href="/' + locale + '/entries/' + 
+      result['id'] + '/edit">Edit</a></li><li><a data-method="delete" href="/' + 
+      locale + '/entries/' + result['id'] + '" rel="nofollow">Delete</a></li>' +
+      '<li><a href="#">Share</a></li></ul></div></div>';
+
       return view;
     };
-
+    // '/assets/green_token_sm.png'
   });
 })();
