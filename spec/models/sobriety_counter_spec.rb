90bf5bedf78ffec39c961d17b0cc599693897b58
Ben Lewis <bennlewis@gmail.com>
2014-02-24 10:29:20 -0700

    WIP tokens change color

--- spec/models/sobriety_counter_spec.rb
+++ spec/models/sobriety_counter_spec.rb
@@ -2,32 +2,46 @@ require 'spec_helper'
 
 describe SobrietyCounter do
   it 'should give back the proper color icon for one day sober' do
-    expected = "blue_token_sm.png"
+    expected = "silver_token_sm.png"
     result = SobrietyCounter.token_for(Time.now)
     expect(result).to eq(expected)
   end
 
-  it 'should give back the proper color icon for ten days sober' do
-    expected = "green_token_sm.png"
-    result = SobrietyCounter.token_for(Date.today - 10)
-    expect(result).to eq(expected)
-  end
-
-  it 'should give back the proper color icon for thirty days sober' do
+  xit 'should give back the proper color icon for one month sober' do
     expected = "red_token_sm.png"
-    result = SobrietyCounter.token_for(Date.today - 30)
+    result = SobrietyCounter.token_for(DateTime.now - 1.month)
     expect(result).to eq(expected)
   end
 
-  it 'should give back the proper color icon for ninety days sober' do
-    expected = "orange_token_sm.png"
-    result = SobrietyCounter.token_for(Date.today - 90)
+  xit 'should give back the proper color icon for two months sober' do
+    expected = "gold_token_sm.png"
+    result = SobrietyCounter.token_for(DateTime.now - 2.months)
     expect(result).to eq(expected)
   end
 
-  it 'should give back the proper color icon for more than a year sober' do
-    expected = "light_green_token_sm.png"
-    result = SobrietyCounter.token_for(Date.today - 366)
+  xit 'should give back the proper color icon for three months sober' do
+    expected = "emerald_token_sm.png"
+    result = SobrietyCounter.token_for(DateTime.now - 3.months)
     expect(result).to eq(expected)
   end
+
+  xit 'should give back the proper color icon for six months sober' do
+    expected = "blue_token_sm.png"
+    result = SobrietyCounter.token_for(DateTime.now - 6.months)
+    expect(result).to eq(expected)
+  end
+
+  xit 'should give back the proper color icon for nine months sober' do
+    # expected = "purple_token_sm.png"
+    result = SobrietyCounter.token_for(DateTime.now - 9.months)
+    expect(result).to eq(expected)
+  end
+
+  xit 'should give back the proper color icon for more than one year sober' do
+    # expected = "bronze_token_sm.png"
+    result = SobrietyCounter.token_for(DateTime.now - 1.year)
+    expect(result).to eq(expected)
+  end
+
+
 end
