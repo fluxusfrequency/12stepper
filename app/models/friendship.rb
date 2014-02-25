a5f824e455c32d1ba66dae37eb4df1bb66c578ff
Ben Lewis <bennlewis@gmail.com>
2014-02-25 16:41:18 -0700

    travis?

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,5 @@
 class Friendship < ActiveRecord::Base
-  include InvalidatesCache
+  include InvalidatesCache unless ENV['TRAVIS']
   validates_presence_of :user_id, :friend_id, :status
   validates_inclusion_of :status, in: %w(pending approved)
   belongs_to :user
