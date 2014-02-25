8d99881b0532b05133c903dbb9c3238ee4d586d5
Ben Horne <benhorne44@gmail.com>
2014-02-25 10:16:49 -0700

    hide map when clicking on hide map link

--- app/assets/javascripts/meetings.js
+++ app/assets/javascripts/meetings.js
@@ -1,92 +1,109 @@
 (function() {
-$(function() {
+  $(function() {
 
-    var queryString, queryData;
-  $('#search-button').click(function(e) {
-    e.preventDefault();
-    queryString = $("#search-form").val();
-    queryData = { address : queryString };
-    $.ajax({
-      url: '/meetings/search',
-      type: 'GET',
-      dataType: 'json',
-      data: queryData,
-      success: function(response) {
-        console.log('hehehehehe')
-        // clearMarkers();
-        // for (var meeting in response) {
-        //   console.log(response[meeting]);
-        //   addToMap(response[meeting]);
-        // }
-      },
-      error: function(response) {
-        var errors = response.responseJSON;
-        if(errors) {
-          $('#flash-section').html(errors);
+      var queryString, queryData;
+    $('#search-button').click(function(e) {
+      e.preventDefault();
+      queryString = $("#search-form").val();
+      queryData = { address : queryString };
+      $.ajax({
+        url: '/meetings/search',
+        type: 'GET',
+        dataType: 'json',
+        data: queryData,
+        success: function(response) {
+          console.log('hehehehehe')
+          // clearMarkers();
+          // for (var meeting in response) {
+          //   console.log(response[meeting]);
+          //   addToMap(response[meeting]);
+          // }
+        },
+        error: function(response) {
+          var errors = response.responseJSON;
+          if(errors) {
+            $('#flash-section').html(errors);
+          }
         }
-      }
-    }).done(function(response) {
-      addResults(response, queryString);
+      }).done(function(response) {
+        addResults(response, queryString);
+      });
     });
+
   });
 
-});
-
-var addResults = function(response, queryString) {
-  $('#search-results').html('');
-  if (response.length === 0) {
-    $('#search-results').append("Sorry, no meetings matched your search.");
-  } else {
-    for (var meeting in response) {
-      var meetingName = '<h3>' + response[meeting]["name"] + '</h3>';
-      var mapLink = '<a class="view-map-link" href="#">view map</a>'
-      var meetingLocation = '<p>Location: ' + response[meeting]["location"] + '</p>';
-      var meetingAddress = '<p>Address: ' + response[meeting]["address"] + '</p>';
-      var meetingDay = '<p>Day: ' + response[meeting]["day"] + '</p>';
-      var meetingTime = '<p>Time: ' + response[meeting]["time"] + '</p>';
-      var meetingData = '<h3>Meetings for <em>' + queryString + '</em></h3><div class="individual-search-result col-lg-12">' + meetingName + mapLink + meetingLocation + meetingAddress + meetingDay + meetingTime + '</div>'
-      $('#search-results').append(meetingData);
+  var addResults = function(response, queryString) {
+    $('#search-results').html('');
+    if (response.length === 0) {
+      $('#search-results').append("Sorry, no meetings matched your search.");
+    } else {
+      for (var meeting in response) {
+        var meetingName = '<h3>' + response[meeting]["name"] + '</h3>';
+        var mapLink = '<a class="view-map-link" href="#">view map</a>'
+        var meetingLocation = '<p>Location: ' + response[meeting]["location"] + '</p>';
+        var meetingAddress = '<p>Address: ' + response[meeting]["address"] + '</p>';
+        var meetingDay = '<p>Day: ' + response[meeting]["day"] + '</p>';
+        var meetingTime = '<p>Time: ' + response[meeting]["time"] + '</p>';
+        var meetingData = '<h3>Meetings for <em>' + queryString + '</em></h3><div class="individual-search-result col-lg-12">' + meetingName + mapLink + meetingLocation + meetingAddress + meetingDay + meetingTime + '</div>'
+        $('#search-results').append(meetingData);
+      };
     };
   };
-};
 
 
-var markers = [];
+  var markers = [];
 
-var addToMap = function(meeting) {
-  var marker = L.mapbox.markerLayer({
-    // this feature is in the GeoJSON format: see geojson.org
-    // for the full specification
-    type: 'Feature',
-    geometry: {
-        type: 'Point',
-        // coordinates here are in longitude, latitude order because
-        // x, y is the standard for GeoJSON and many formats
-        coordinates: [parseFloat(meeting["lng"]), parseFloat(meeting["lat"])]
-    },
-    properties: {
-        title: meeting["name"],
-        description: meeting["address"],
-        // one can customize markers by adding simplestyle properties
-        // http://mapbox.com/developers/simplestyle/
-        'marker-size': 'large',
-        'marker-color': '#f0a'
-        // 'marker-symbol': counter
+  var addToMap = function(meeting) {
+    var marker = L.mapbox.markerLayer({
+      // this feature is in the GeoJSON format: see geojson.org
+      // for the full specification
+      type: 'Feature',
+      geometry: {
+          type: 'Point',
+          // coordinates here are in longitude, latitude order because
+          // x, y is the standard for GeoJSON and many formats
+          coordinates: [parseFloat(meeting["lng"]), parseFloat(meeting["lat"])]
+      },
+      properties: {
+          title: meeting["name"],
+          description: meeting["address"],
+          // one can customize markers by adding simplestyle properties
+          // http://mapbox.com/developers/simplestyle/
+          'marker-size': 'large',
+          'marker-color': '#f0a'
+          // 'marker-symbol': counter
+      }
+    });
+    markers.push(marker);
+    marker.addTo(map);
+  };
+
+
+  var clearMarkers = function() {
+    for (var m in markers) {
+      markers[m].clearLayers();
     }
-  });
-  markers.push(marker);
-  marker.addTo(map);
-  // setTimeout(function() {
+  };
 
-  //     // wait 1 second before announcing the result
-  //   }, 1000);
-    console.log(markers);
-};
+  // for any of the view map links, on click, run the function
+  // for(map in $('.view-map-link')) {
+    
+    $('.view-map-link').on('click', function() {
+      if ($('.map-container').hasClass('hide-map-container')) {
+        // clear map marker
+        // add new
+      } else {
+        $('.map-container').addClass('hide-map-container')
+      }
+    })
+  // }
 
-
-var clearMarkers = function() {
-  for (var m in markers) {
-    markers[m].clearLayers();
-  }
-};
+  $('.hide-map-link').on('click', function() {
+    if ($('.map-container').hasClass('hide-map-container')) {
+      // clear map marker
+      // add new
+    } else {
+      $('.map-container').addClass('hide-map-container')
+    }
+  })
 })();
