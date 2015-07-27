6a7f59fca96ab35d2e25d3ba1613cf85d6b2b91d
Ben Lewis <bennlewis@gmail.com>
2015-07-27 16:37:37 -0600

    dockerize

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20140226030605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "big_book_chapter_translations", force: true do |t|
+  create_table "big_book_chapter_translations", force: :cascade do |t|
     t.integer  "big_book_chapter_id", null: false
     t.string   "locale",              null: false
     t.datetime "created_at"
@@ -28,13 +28,13 @@ ActiveRecord::Schema.define(version: 20140226030605) do
   add_index "big_book_chapter_translations", ["big_book_chapter_id"], name: "index_big_book_chapter_translations_on_big_book_chapter_id", using: :btree
   add_index "big_book_chapter_translations", ["locale"], name: "index_big_book_chapter_translations_on_locale", using: :btree
 
-  create_table "big_book_chapters", force: true do |t|
+  create_table "big_book_chapters", force: :cascade do |t|
     t.string  "title"
     t.text    "body"
     t.integer "chapter_number"
   end
 
-  create_table "ckeditor_assets", force: true do |t|
+  create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
     t.string   "data_content_type"
     t.integer  "data_file_size"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20140226030605) do
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
 
-  create_table "entries", force: true do |t|
+  create_table "entries", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.integer  "user_id"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 20140226030605) do
 
   add_index "entries", ["user_id"], name: "index_entries_on_user_id", using: :btree
 
-  create_table "friendships", force: true do |t|
+  create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
     t.string   "status",     default: "pending"
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 20140226030605) do
   add_index "friendships", ["friend_id"], name: "index_friendships_on_friend_id", using: :btree
   add_index "friendships", ["user_id"], name: "index_friendships_on_user_id", using: :btree
 
-  create_table "statuses", force: true do |t|
+  create_table "statuses", force: :cascade do |t|
     t.integer  "user_id"
     t.text     "content"
     t.datetime "created_at"
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 20140226030605) do
 
   add_index "statuses", ["user_id"], name: "index_statuses_on_user_id", using: :btree
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
@@ -93,7 +93,7 @@ ActiveRecord::Schema.define(version: 20140226030605) do
     t.text     "bio",             default: "", null: false
   end
 
-  create_table "worksheet_translations", force: true do |t|
+  create_table "worksheet_translations", force: :cascade do |t|
     t.integer  "worksheet_id", null: false
     t.string   "locale",       null: false
     t.datetime "created_at"
@@ -105,7 +105,7 @@ ActiveRecord::Schema.define(version: 20140226030605) do
   add_index "worksheet_translations", ["locale"], name: "index_worksheet_translations_on_locale", using: :btree
   add_index "worksheet_translations", ["worksheet_id"], name: "index_worksheet_translations_on_worksheet_id", using: :btree
 
-  create_table "worksheets", force: true do |t|
+  create_table "worksheets", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.string   "locale"
